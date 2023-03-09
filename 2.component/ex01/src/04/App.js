f9603a142554ab0fef32de53bfd112d0ae9c0577
yuming0315 <gjdbal111@naver.com>
2023-03-09 09:40:17 +0900

    component Property

--- 2.component/ex01/src/04/App.js
+++ 2.component/ex01/src/04/App.js
@@ -4,7 +4,16 @@ import MyComponent from "./MyComponent";
 function App() {
   return (
     <div id="App">
-      <MyComponent props02={10} />
+      <MyComponent
+        props02={10}
+        props03={true}
+        props04={{ no: 1, name: "dooly" }}
+        props05={[1, 2, 3, 4, 5]}
+        props06={() => "함수"}
+        props07={10}
+        props08={[true, false, true, false]}
+        props09={{ no: 1, name: "dooly", email: "dooly@gmail.com" }}
+      />
     </div>
   );
 }
--- 2.component/ex01/src/04/MyComponent.js
+++ 2.component/ex01/src/04/MyComponent.js
@@ -1,7 +1,17 @@
 import React, { Fragment } from "react";
 import { PropTypes } from "prop-types";
 
-function MyComponent({ props01, props02 }) {
+function MyComponent({
+  props01,
+  props02,
+  props03,
+  props04,
+  props05,
+  props06,
+  props07,
+  props08,
+  props09,
+}) {
   return (
     <Fragment>
       <h2>Property Validation</h2>
@@ -11,19 +21,68 @@ function MyComponent({ props01, props02 }) {
 
       <span>props02: {props02 ? props02 : "--- not set ---"}</span>
       <br />
+
+      <span>props03: {props03 ? `${props03}` : "--- not set ---"}</span>
+      <br />
+      {
+        // true의 경우 화면에 찍어내지 않음 템플릿 리터럴 써줘야함 이럴땐
+      }
+      <span>props04: {props04 ? props04.name : "--- not set ---"}</span>
+      <br />
+      <span>
+        props05:{" "}
+        {props05.map((e, idx) => (
+          <b key={idx}>{e}</b>
+        ))}
+      </span>
+      <br />
+      <span>props06: {props06 ? props06() : "--- not set ---"}</span>
+      <br />
+      <span>props07: {props07 ? props07 : "--- not set ---"}</span>
+      <br />
+      <span>
+        props08:{" "}
+        {props08.map((e, idx) => (
+          <b key={idx}>{`${e}`}</b>
+        ))}
+      </span>
+      <br />
+
+      <span>
+        props09: {props09 ? JSON.stringify(props09) : "--- not set ---"}
+      </span>
     </Fragment>
   );
 }
 
 // property type and required
 MyComponent.propTypes = {
+  // JavaScript Data Type
   props01: PropTypes.string,
   props02: PropTypes.number.isRequired,
+  props03: PropTypes.bool.isRequired,
+  props04: PropTypes.object.isRequired,
+  props05: PropTypes.array.isRequired,
+  props06: PropTypes.func.isRequired,
+
+  // Combined with JavaScript Data Type
+  props07: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  props08: PropTypes.arrayOf(PropTypes.bool).isRequired,
+  props09: PropTypes.shape({
+    no: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 // property default value
 MyComponent.defaultProps = {
   props01: "기본값",
+  props02: 10,
+  props03: false,
+  props04: {},
+  props05: [],
+  props06: () => {}, // dummy function
 };
 
 export default MyComponent;
