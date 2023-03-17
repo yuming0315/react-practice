dfcf673d628928405f72e5452c6ad6305cfb62af
yuming0315 <gjdbal111@naver.com>
2023-03-17 13:27:07 +0900

    kanbanBoard

--- kanbanboard/frontend/src/App.js
+++ kanbanboard/frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./assets/css/App.css";
-import KanbanBoard from "./Kanbanboard";
+import KanbanBoard from "./KanbanBoard";
 
 export default function App(props) {
   return (
